a5d101ed6abe6e7fe0b9aa155a95a283963f1a59
Henry Huang <Mchighlight@gmail.com>
2020-10-30 19:09:28 -0400

    ger serverside prop

--- pages/secret.js
+++ pages/secret.js
@@ -6,6 +6,7 @@ import BasePage from '@/components/BasePage';
 import withAuth from '@/hoc/withAuth';
 
 const Secret = ({user, loading}) => {
+  debugger
   return (
     <BaseLayout user={user} loading={loading}>
       <BasePage>
