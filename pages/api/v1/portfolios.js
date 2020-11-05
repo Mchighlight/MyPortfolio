aebae215da17a99404f09e9541995e4a262509ee
Henry Huang <Mchighlight@gmail.com>
2020-11-05 13:58:19 -0500

    create access token

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -1,13 +1,14 @@
 import axios from 'axios' ;
 import PortfolioApi from '@/lib/api/portfolios' ;
+import auth0 from '@/utils/auth0';
 
 export default async function createPortfolio(req, res){
     try{
-        const data = req.body ;
-        await new PortfolioApi().createPortfolio(data);
+        const { accessToken } = await auth0.getSession(req);
+        console.log(accessToken)
+        await new PortfolioApi().createPortfolio(req.body);
         return res.json({message: "Portfolio was created"}) ;
     } catch(e){
-        console.log("SHIT")
         return res.status(e.status || 400).end(e.message) ;
     }
 }
