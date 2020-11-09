4d1cc301a9edd222cc562c4e76b529884695cf3e
Henry Huang <Mchighlight@gmail.com>
2020-11-08 23:33:29 -0500

    Get initial conent to blog update

--- pages/blogs/editor/[id].js
+++ pages/blogs/editor/[id].js
@@ -1,18 +1,27 @@
+
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import withAuth from '@/hoc/withAuth';
-import { Editor } from 'slate-simple-editor';
+import { Editor } from 'slate-simple-editor/src/lib/index';
 import { toast } from 'react-toastify';
+import { useGetBlog } from 'actions/blogs';
+import { useRouter } from 'next/router';
 
 const BlogUpdateEditor = ({user, loading}) => {
+  const router = useRouter();
+  const { data } = useGetBlog(router.query.id);
+  debugger
 
   return (
     <BaseLayout user={user} loading={loading}>
       <BasePage>
-        <Editor
-          header="Update Your Blog..."
-          onSave={() => {}}
-        />
+        { data && data.content &&
+          <Editor
+            header="Update Your Blog..."
+            initialContent={data.content}
+            onSave={() => {}}
+          />
+        }
       </BasePage>
     </BaseLayout>
   )
