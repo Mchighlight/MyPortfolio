36123a176e80413766c177b7878defa4bdacb04e
Henry Huang <Mchighlight@gmail.com>
2020-11-13 16:11:58 -0500

    dashboard page

--- pages/blogs/editor/[id].js
+++ pages/blogs/editor/[id].js
@@ -4,27 +4,38 @@ import BasePage from '@/components/BasePage';
 import withAuth from '@/hoc/withAuth';
 import { Editor } from 'slate-simple-editor/src/lib/index';
 import { toast } from 'react-toastify';
-import { useGetBlog } from 'actions/blogs';
+import { useGetBlog, useUpdateBlog } from 'actions/blogs';
 import { useRouter } from 'next/router';
 
 const BlogUpdateEditor = ({user, loading}) => {
-  const router = useRouter();
-  const { data } = useGetBlog(router.query.id);
-  debugger
-
-  return (
-    <BaseLayout user={user} loading={loading}>
-      <BasePage>
-        { data && data.content &&
-          <Editor
-            header="Update Your Blog..."
-            initialContent={data.content}
-            onSave={() => {}}
-          />
-        }
-      </BasePage>
-    </BaseLayout>
-  )
-}
-
-export default withAuth(BlogUpdateEditor)('admin');
+    const router = useRouter();
+    const { data } = useGetBlog(router.query.id);
+    const [updateBlog, {error, loading: isBlogSaving}] = useUpdateBlog();
+  
+    const _updateBlog = async data => {
+      await updateBlog(router.query.id, data);
+      toast.success('Blog updated!')
+    }
+  
+    if (error) {
+      toast.error(error)
+    }
+  
+    return (
+      <BaseLayout user={user} loading={loading}>
+        <BasePage>
+          { data && data.content &&
+            <Editor
+              header="Update Your Blog..."
+              initialContent={data.content}
+              onSave={_updateBlog}
+              loading={isBlogSaving}
+            />
+          }
+        </BasePage>
+      </BaseLayout>
+    )
+  }
+  
+  export default withAuth(BlogUpdateEditor)('admin');
+  
