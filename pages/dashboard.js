776d57a68e151b7cd583b0ad6ad8b59e25181414
Henry Huang <Mchighlight@gmail.com>
2020-11-13 16:44:26 -0500

    dropdown in dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,12 +4,36 @@ import BasePage from '@/components/BasePage';
 import { withAuth } from 'utils/auth0';
 import { Row, Col } from 'reactstrap';
 import Masthead from 'components/shared/Masthead';
+import PortDropdown from 'components/shared/Dropdown';
+import Link from 'next/link';
 
 import auth0 from 'utils/auth0';
 import BlogApi from 'lib/api/blogs';
 
 const Dashboard = ({user, blogs}) => {
-  debugger
+
+  const createOptions = (blog) => {
+
+    return [
+      {key: `${blog._id}-published`, text: 'Published', handlers: { onClick: () => {alert(`Clicking Publish! ${blog._id}`)}}},
+      {key: `${blog._id}-delete`,text: 'Delete', handlers: { onClick: () => {alert(`Clicking Delete! ${blog._id}`)}}}
+    ]
+  }
+
+  const renderBlogs = (blogs, status) => (
+    <ul className="user-blogs-list">
+      { blogs.filter(blog => blog.status === status).map(blog =>
+        <li key={blog._id}>
+          <Link href="/blogs/editor/[id]" as={`/blogs/editor/${blog._id}`}>
+            <a>{blog.title}</a>
+          </Link>
+          <PortDropdown items={createOptions(blog)} />
+        </li>
+        )
+      }
+    </ul>
+  )
+
   return (
     <BaseLayout navClass="transparent" user={user} loading={false}>
       <Masthead imagePath="/images/home-bg.jpg" />
@@ -17,9 +41,11 @@ const Dashboard = ({user, blogs}) => {
         <Row>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Published Blogs </h2>
+            {renderBlogs(blogs, 'published')}
           </Col>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Draft Blogs </h2>
+            {renderBlogs(blogs, 'draft')}
           </Col>
         </Row>
       </BasePage>
@@ -32,5 +58,4 @@ export const getServerSideProps = withAuth(async ({req, res}) => {
   const json = await new BlogApi(accessToken).getByUser();
   return { blogs: json.data }
 })('admin');
-
 export default Dashboard;
