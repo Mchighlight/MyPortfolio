6ad319ec3434e733fd1371d526f2307101ae877d
Henry Huang <Mchighlight@gmail.com>
2020-11-18 22:03:06 -0500

    Flip the Card

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,16 +1,37 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
+import { Row, Col } from 'reactstrap';
+import { SlateView } from 'slate-simple-editor/src/lib/index';
 import { useGetUser } from '@/actions/user';
 
-const BlogDetail = () => {
+import BlogApi from 'lib/api/blogs';
+
+const BlogDetail = ({blog}) => {
   const { data, loading } = useGetUser();
   return (
     <BaseLayout user={data} loading={loading}>
-      <BasePage>
-        <h1>I am Blog Detail</h1>
-      </BasePage>
+        <BasePage className="slate-container">
+            <Row>
+            <Col md={{size: 8, offset: 2}}>
+                <SlateView initialContent={blog.content}/>
+            </Col>
+            </Row>
+        </BasePage>
     </BaseLayout>
   )
+}
+
+
+export async function getStaticPaths() {
+    const json = await new BlogApi().getAll();
+    const blogs = json.data;
+    const paths = blogs.map(b => ({params: { slug: b.slug}}));
+    return { paths, fallback: false};
+  }
+  
+export async function getStaticProps({params}) {
+const json = await new BlogApi().getBySlug(params.slug);
+return {props: {blog: json.data}}
 }
 
 export default BlogDetail;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,33 @@
+
+import { useState, useRef, useEffect } from 'react';
 import BaseLayout from '@/components/layouts/BaseLayout';
 import { Container, Row, Col } from 'reactstrap';
 import Typed from 'react-typed';
-import { useGetUser } from '@/actions/user' ;
+import { useGetUser } from '@/actions/user';
 
-const ROLES = ['Developer', 'Data Engineer', 'Team Player', 'Cloud Computing', 'Python', 'SQL'];;
+const ROLES = ['Developer', 'Data Engineer', 'Team Player', 'Cloud Computing', 'Python', 'SQL'];
 const Index = () => {
+  const [isFlipping, setIsFlipping] = useState(false);
+  const { data, loading } = useGetUser();
+  const flipInterval = useRef();
 
-  const { data, loading } = useGetUser() ;
-   
+  useEffect(() => {
+    startAnimation();
+    return () => flipInterval.current && clearInterval(flipInterval.current)
+  }, []);
+
+  const startAnimation = () => {
+    flipInterval.current = setInterval(() => {
+      setIsFlipping(prevFlipping => !prevFlipping);
+    }, 20000);
+  }
+
   return (
-    <BaseLayout 
-    className="cover" 
-    user = {data} 
-    loading= {loading}
-    navClass = "transparent"
-    >
-      
+    <BaseLayout
+      user={data}
+      loading={loading}
+      navClass="transparent"
+      className={`cover ${isFlipping ? 'cover-orange' : 'cover-blue'}`}>
       <div className="main-section">
         <div className="background-image">
           <img src="/images/background-index.png" />
@@ -24,10 +36,10 @@ const Index = () => {
           <Row>
             <Col md="6">
               <div className="hero-section">
-                <div className={`flipper`}>
-                  <div className="back">
+                <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
+                  <div className="front">
                     <div className="hero-section-content">
-                      <h2> Data Engineer </h2>
+                      <h2> Full Stack Web Developer </h2>
                       <div className="hero-section-content-intro">
                         Have a look at my portfolio and job history.
                       </div>
@@ -37,14 +49,26 @@ const Index = () => {
                       <div className="shadow-inner"> </div>
                     </div>
                   </div>
+                  <div className="back">
+                    <div className="hero-section-content">
+                      <h2> Data Engineer and Software Engineer </h2>
+                      <div className="hero-section-content-intro">
+                        Have a look at my portfolio and job history.
+                      </div>
+                    </div>
+                    <img className="image" src="/images/section-2.png"/>
+                    <div className="shadow-custom shadow-custom-orange">
+                      <div className="shadow-inner"> </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </Col>
             <Col md="6" className="hero-welcome-wrapper">
               <div className="hero-welcome-text">
                 <h1>
-                  Welcome to the portfolio website of Henry Huang.
-                  Get informed, collaborate and discover projects I was working on through the years!
+                Welcome to the portfolio website of Henry Huang.
+Get informed, collaborate and discover projects I was working on through the years!
                 </h1>
               </div>
               <Typed
