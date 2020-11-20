af5be0526b3d2a3e3cd4a2b65a63cf6481402e7f
Henry Huang <Mchighlight@gmail.com>
2020-11-20 03:56:32 -0500

    portfolio detail data

--- components/BasePage.js
+++ components/BasePage.js
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import { Container } from 'reactstrap';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -23,6 +24,7 @@ const BasePage = props => {
     children } = props;
 
   const pageType = indexPage ? 'index-page' : 'base-page';
+  const Wrapper = noWrapper ? React.Fragment : Container;
   return (
     <>
       <Head>
@@ -43,18 +45,10 @@ const BasePage = props => {
           href={`${process.env.BASE_URL}${canonicalPath ? canonicalPath : router.asPath}`} />
       </Head>
       <div className={`${pageType} ${className}`}>
-        { noWrapper &&
-          <>
-            { header && <PageHeader header={header} /> }
-            {children}
-          </>
-        }
-        { !noWrapper &&
-          <Container>
-            { header && <PageHeader header={header} /> }
-            {children}
-          </Container>
-        }
+        <Wrapper>
+          { header && <PageHeader header={header} /> }
+          {children}
+        </Wrapper>
       </div>
     </>
   )
