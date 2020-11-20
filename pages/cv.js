1a4cf0369f0a9dc9eb960821f4d2b426c3a71c03
Henry Huang <Mchighlight@gmail.com>
2020-11-20 02:39:53 -0500

    Index Page Type

--- pages/cv.js
+++ pages/cv.js
@@ -9,7 +9,8 @@ const Cv = () => {
   const { data, loading } = useGetUser() ;
   return (
     <BaseLayout user={data} loading={loading} >
-      <BasePage>
+      <BasePage
+        title="My Experiences - Hung-Chih Huang">
       <Row>
           <Col md={{size: 8, offset: 2}}>
             <iframe style={{width: '100%', height: '800px'}} src="/Hung-Chih_Huang.pdf"/>
